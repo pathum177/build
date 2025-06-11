894ea45463adcefd6909d06516c6e683f46dcf7b
pathum177 <pathummalsara177@gmail.com>
2025-06-11 23:50:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SUPUN-MD=LIMyyAoR#-GkSTuiM",
-HACKER : process.env.HACKER || "947yyyy"
+SESSION_ID: process.env.SESSION_ID || "mUQHQZhR#CXzwIL1um8sRwH0rL1krUXc31RI-O6BTXCuP7w7gW9k",
+HACKER : process.env.HACKER || "94723975388"
 };
