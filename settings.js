2a566494d07f1fffa0e16ef2abfbe85ca694231d
pathum177 <pathummalsara177@gmail.com>
2025-06-11 23:38:36 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,26 @@
+
+/* 
+██╗░░░██╗██████╗░███╗░░░███╗░█████╗░██████╗░███████╗ 
+██║░░░██║██╔══██╗████╗░████║██╔══██╗██╔══██╗╚════██║ 
+██║░░░██║██║░░██║██╔████╔██║██║░░██║██║░░██║░░███╔═╝ 
+██║░░░██║██║░░██║██║╚██╔╝██║██║░░██║██║░░██║██╔══╝░░ 
+╚██████╔╝██████╔╝██║░╚═╝░██║╚█████╔╝██████╔╝███████╗ 
+░╚═════╝░╚═════╝░╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚══════╝ 
+By UDMODZ
+DONT SELL
+A FREE HACK
+I'M UDMODZ
+
+
+*/
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "SUPUN-MD=LIMyyAoR#-GkSTuiM",
+HACKER : process.env.HACKER || "947yyyy"
+};
